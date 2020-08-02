88bb3bc01cf7abe156fd638dcf23e0f1cf5ef609
AJ Thammana <avthammana@wpi.edu>
2020-08-02 03:55:43 -0400

    Updates

---
+++ precache-manifest.c0a6637231d39d873e91f7c2425f0850.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eea254183af2b0a12228b3323ac4f38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f5fa522da3112f695d3e",
+    "url": "/portfolio/static/css/main.32c4b322.chunk.css"
+  },
+  {
+    "revision": "48f62cb53a9e53666afb",
+    "url": "/portfolio/static/js/2.c4b98808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c4b98808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fa522da3112f695d3e",
+    "url": "/portfolio/static/js/main.f1c46e57.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "00478779576256af2c863221da3450b1",
+    "url": "/portfolio/static/media/BoozPic.00478779.png"
+  },
+  {
+    "revision": "c1900c3fda64720304570856e18c4fa5",
+    "url": "/portfolio/static/media/ppp.c1900c3f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3a0c14c192a02104484aecbee41f8e84.js"
+  "/portfolio/precache-manifest.c0a6637231d39d873e91f7c2425f0850.js"
 );
 
 self.addEventListener('message', (event) => {

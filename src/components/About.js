26fb1bc140b00ae15a8da90db95b3ee1d96e5b3c
athammana <aj@virpietech.com>
2020-09-21 16:30:12 -0400

    Removed warnings

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { PieChart } from 'react-minimal-pie-chart';
 import SkillsTable from './SkillsTable';
 // import { Button } from 'react-scroll';
 // import ReactDOM from 'react-dom';
@@ -30,22 +29,6 @@ class About extends React.Component{
         <div id='aboutbg' className='about-background'/>
         <div className='about-content'>
           <div className='left-card card container'>
-            {/* <h1 style={{textAlign: 'center'}}>
-              Experience
-            </h1>
-            <h3 style={{textAlign: 'center'}}>
-              What I've Done
-            </h3>
-            <PieChart style={{width:'40vw', height:'60vh', marginTop:'-12vh'}} radius='25' animate='True' 
-              label={({ dataEntry }) => dataEntry.title}
-              labelStyle={(index) => ({
-                fontSize: '4px',
-                fontFamily: 'Montserrat',
-              })}
-              labelPosition={65}
-              data={this.state.pieData}
-            />
-            <Button style={{position:'fixed', cursor:'pointer', bottom:'5vh', right:'5vh', width:'40px', height:'40px', backgroundColor:'blue', borderRadius:'50%', border: '0px'}} onClick={this.break}/> */}
             <h1 style={{textAlign: 'center'}}>
               Experience
             </h1>

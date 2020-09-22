dab13b96101aec51111c77b1389387facca48a33
athammana <aj@virpietech.com>
2020-09-22 10:38:37 -0400

    Updates

---
+++ precache-manifest.45a884f382a5810a784cbeeaf69861f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e42d28d1e075bead88c616993d939d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a5f5fabca2f7a80aa333",
+    "url": "/portfolio/static/css/main.ffed5497.chunk.css"
+  },
+  {
+    "revision": "74362495b9b564c88dd4",
+    "url": "/portfolio/static/js/2.693c3d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.693c3d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f5fabca2f7a80aa333",
+    "url": "/portfolio/static/js/main.11666d8c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "00478779576256af2c863221da3450b1",
+    "url": "/portfolio/static/media/BoozPic.00478779.png"
+  },
+  {
+    "revision": "87b3ae8f33f87b1cc6f13db05b07a867",
+    "url": "/portfolio/static/media/DocTour.87b3ae8f.png"
+  },
+  {
+    "revision": "86fc0a44fc3cc9871a6ff2ec9a8297ab",
+    "url": "/portfolio/static/media/kiwi.86fc0a44.png"
+  },
+  {
+    "revision": "7abbfa31dc69dee1ae942a7046f8c4fd",
+    "url": "/portfolio/static/media/ocr.7abbfa31.jpg"
+  },
+  {
+    "revision": "c1900c3fda64720304570856e18c4fa5",
+    "url": "/portfolio/static/media/ppp.c1900c3f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c0a6637231d39d873e91f7c2425f0850.js"
+  "/portfolio/precache-manifest.45a884f382a5810a784cbeeaf69861f2.js"
 );
 
 self.addEventListener('message', (event) => {
